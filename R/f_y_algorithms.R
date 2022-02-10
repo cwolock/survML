f0d875fc2f27c00baf0e17813e68689e1b92b436
Charles Wolock <cwolock@gmail.com>
2022-02-09 16:36:40 -0800

    more tuning

--- R/f_y_algorithms.R
+++ R/f_y_algorithms.R
@@ -205,7 +205,7 @@ f_y_stackSLcdf <- function(time, event, X, censored, bin_size, isotonize = TRUE,
   X <- stacked[,-ncol(stacked)]
 
 
-  tune = list(ntrees = c(1000, 5000), max_depth = c(2,3, 4), minobspernode = 10,
+  tune = list(ntrees = c(1000, 5000, 10000), max_depth = c(2,3, 4), minobspernode = 10,
               shrinkage = c(0.01, 0.1, 0.5))
   xgb_grid = SuperLearner::create.SL.xgboost(tune = tune, detailed_names = TRUE)
   fit <- SuperLearner::SuperLearner(Y = Y,
