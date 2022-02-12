4149c21af9d53c119fdcf72faee862893b75357d
Charles Wolock <cwolock@gmail.com>
2022-02-11 22:58:01 -0800

    fixed issue with product integral calc

--- R/compute_prodint.R
+++ R/compute_prodint.R
@@ -24,18 +24,23 @@ compute_prodint <- function(cdf_uncens,
     # get S_Y estimates up to t
     S_Y_1_curr <- cdf_uncens[1:curr_length]
     S_Y_0_curr <- cdf_cens[1:curr_length]
-    #S_Y_curr <- cdf_marg[1:curr_length]
 
     dF_Y_1_pred <- c(S_Y_1_curr[1], diff(S_Y_1_curr))
 
     S_Y_1_pred_left <- c(1, 1-S_Y_1_curr[-length(S_Y_1_curr)])# probability of being "at risk" at time t
     ### CHECK TO MAKE SURE THIS IS CORRECT WITH THE DISCRETIZATION OF TIME
     S_Y_0_pred_left <- c(1, 1-S_Y_0_curr[-length(S_Y_0_curr)])# probability of being "at risk" at time t
-    #S_Y_pred_left <- c(1, 1-S_Y_curr[-length(S_Y_curr)])
+
+    if (denom_method != "conditional"){
+      S_Y_curr <- cdf_marg[1:curr_length]
+      S_Y_pred_left <- c(1, 1-S_Y_curr[-length(S_Y_curr)])
+      low <- S_Y_pred_left
+    }
+
 
     low_left <- S_Y_1_pred_left * p_uncens
     low_right <- S_Y_0_pred_left * (1 - p_uncens)
-    #low <- S_Y_pred_left
+
     # product form
     if (denom_method == "conditional"){
       S_T_est <- prod(1 - p_uncens * dF_Y_1_pred/(low_left + low_right))
