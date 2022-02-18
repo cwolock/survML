d84767bbc98a8c236d111018c841c70d8e34fa57
Charles Wolock <cwolock@gmail.com>
2022-02-17 16:45:59 -0800

    added first time point (0) to stacked cdf

--- R/stack.R
+++ R/stack.R
@@ -7,9 +7,15 @@ stack <- function(time, X, time_grid){
   # we will treat time as continuous
   ncol_stacked <- ncol(X) + 2 # covariates, time, binary outcome
   stacked <- matrix(NA, ncol = ncol_stacked, nrow = 1)
-  for (i in 1:(length(trunc_time_grid))){ # can change this to not do anything in last time bin
-    event_indicators <- matrix(ifelse(time <= time_grid[i + 1], 1, 0))
-    t <- time_grid[i + 1]
+  # for (i in 1:(length(trunc_time_grid))){ # can change this to not do anything in last time bin
+  #   event_indicators <- matrix(ifelse(time <= time_grid[i + 1], 1, 0))
+  #   t <- time_grid[i + 1]
+  #   newdata <- as.matrix(cbind(t, X, event_indicators))
+  #   stacked <- rbind(stacked, newdata)
+  # }
+  for (i in 1:(length(time_grid))){ # can change this to not do anything in last time bin
+    event_indicators <- matrix(ifelse(time <= time_grid[i], 1, 0))
+    t <- time_grid[i]
     newdata <- as.matrix(cbind(t, X, event_indicators))
     stacked <- rbind(stacked, newdata)
   }
