6075195dd10c03da8a39c80431d4efbf7800ab95
Charles Wolock <cwolock@gmail.com>
2022-02-17 23:01:26 -0800

    added continuous vs discrete stackSurv

--- R/stackSurv.R
+++ R/stackSurv.R
@@ -18,6 +18,7 @@ stackSurv <- function(time,
                       X,
                       newX,
                       newtimes,
+                      time_grid_approx,
                       bin_size = NULL,
                       test_time = NULL,
                       test_event = NULL,
@@ -42,6 +43,8 @@ stackSurv <- function(time,
   trunc_time_grid <- time_grid[-length(time_grid)]
 
   tune <- list(ntrees = c(250, 500, 1000, 2500), max_depth = c(1,2,3),
+               eta = c(0.01))
+  tune <- list(ntrees = c(2000), max_depth = c(1),
                eta = c(0.01))
 
   param_grid <- expand.grid(ntrees = tune$ntrees,
@@ -109,9 +112,9 @@ stackSurv <- function(time,
   opt_eta <- param_grid$eta[opt_param_index]
   opt_params <- list(ntrees = opt_ntrees, max_depth = opt_max_depth, eta = opt_eta)
   #stacked <- conSurv:::stack_haz(time = time, event = event, X = X, time_grid = time_grid)
-  Y <- stacked[,ncol(stacked)]
-  X <- as.matrix(stacked[,-ncol(stacked)])
-  xgmat <- xgboost::xgb.DMatrix(data = X, label = Y)
+  Y2 <- stacked[,ncol(stacked)]
+  X2 <- as.matrix(stacked[,-ncol(stacked)])
+  xgmat <- xgboost::xgb.DMatrix(data = X2, label = Y2)
   fit <- xgboost::xgboost(data = xgmat, objective="binary:logistic", nrounds = opt_ntrees,
                           max_depth = opt_max_depth, eta = opt_eta,
                           verbose = FALSE, nthread = 1,
@@ -138,7 +141,84 @@ stackSurv <- function(time,
   print(fit$params)
   print(fit$niter)
 
-  # this version does dummy variables for time - include this as an option
+
+  # time dummies
+  ncol_stacked <- ncol(X) + length(trunc_time_grid) + 1 # covariates, risk set dummies, binary outcome
+  stacked <- matrix(NA, ncol = ncol_stacked, nrow = 1)
+  for (i in 1:(length(trunc_time_grid))){
+    risk_set <- dat[dat$time > time_grid[i],] # should this be <= rather than <??
+    risk_set_covariates <- risk_set[,1:ncol(X)]
+    event_indicators <- matrix(ifelse(risk_set$time <= time_grid[i + 1 ] & risk_set$event == 1, 1, 0))
+    dummies <- matrix(0, ncol = length(trunc_time_grid), nrow = nrow(risk_set))
+    dummies[,i] <- 1
+    newdata <- as.matrix(cbind(dummies, risk_set_covariates, event_indicators))
+    stacked <- rbind(stacked, newdata)
+  }
+
+  stacked <- stacked[-1,]
+  risk_set_names <- paste0("risk_set_", seq(1, (length(trunc_time_grid))))
+  colnames(stacked)[1:(length(trunc_time_grid))] <- risk_set_names
+  stacked <- data.frame(stacked)
+  CV_risks <- unlist(lapply(1:nrow(param_grid), get_CV_risk))
+  opt_param_index <- which.min(CV_risks)
+  opt_ntrees <- param_grid$ntrees[opt_param_index]
+  opt_max_depth <- param_grid$max_depth[opt_param_index]
+  opt_eta <- param_grid$eta[opt_param_index]
+  opt_params <- list(ntrees = opt_ntrees, max_depth = opt_max_depth, eta = opt_eta)
+  #stacked <- conSurv:::stack_haz(time = time, event = event, X = X, time_grid = time_grid)
+  Y <- stacked[,ncol(stacked)]
+  X <- as.matrix(stacked[,-ncol(stacked)])
+  xgmat <- xgboost::xgb.DMatrix(data = X, label = Y)
+  fit <- xgboost::xgboost(data = xgmat, objective="binary:logistic", nrounds = opt_ntrees,
+                          max_depth = opt_max_depth, eta = opt_eta,
+                          verbose = FALSE, nthread = 1,
+                          save_period = NULL, eval_metric = "logloss")
+  get_hazard_preds <- function(t){
+    dummies <- matrix(0, ncol = length(trunc_time_grid), nrow = nrow(newX))
+    index <- max(which(trunc_time_grid <= t))
+    dummies[,index] <- 1
+    new_stacked <- cbind(dummies, newX)
+    risk_set_names <- paste0("risk_set_", seq(1, (length(trunc_time_grid))))
+    colnames(new_stacked)[1:length(trunc_time_grid)] <- risk_set_names
+    preds <- predict(fit, newdata=new_stacked)
+    return(preds)
+  }
+
+  hazard_preds <- apply(X = matrix(trunc_time_grid), FUN = get_hazard_preds, MARGIN = 1)
+
+  get_surv_preds <- function(t){
+    final_index <- max(which(trunc_time_grid <= t))
+    haz <- as.matrix(hazard_preds[,1:final_index])
+    anti_haz <- 1 - haz
+    surv <- apply(anti_haz, MARGIN = 1, prod)
+    return(surv)
+  }
+
+  surv_preds2 <- apply(X = matrix(newtimes), FUN = get_surv_preds, MARGIN = 1)
+  print(CV_risks)
+  print(fit$params)
+  print(fit$niter)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  # # Super learner version of stacking
+  # # dummies
+  # # this version does dummy variables for time - include this as an option
   # ncol_stacked <- ncol(X) + length(trunc_time_grid) + 1 # covariates, risk set dummies, binary outcome
   # stacked <- matrix(NA, ncol = ncol_stacked, nrow = 1)
   # for (i in 1:(length(trunc_time_grid))){
@@ -155,43 +235,166 @@ stackSurv <- function(time,
   # risk_set_names <- paste0("risk_set_", seq(1, (length(trunc_time_grid))))
   # colnames(stacked)[1:(length(trunc_time_grid))] <- risk_set_names
   # stacked <- data.frame(stacked)
-  # Y <- stacked$event_indicators
-  # X <- stacked[,-ncol(stacked)]
-  #
-  # fit <- SuperLearner::SuperLearner(Y = Y,
-  #                                   X = X,
+  # Y3 <- stacked$event_indicators
+  # X3 <- stacked[,-ncol(stacked)]
+  # # fit_dum <- SuperLearner::SuperLearner(Y = Y3,
+  # #                                   X = X3,
+  # #                                   family = binomial(),
+  # #                                   SL.library = SL.library,
+  # #                                   method = "method.NNLS",
+  # #                                   verbose = FALSE)
+  # tune = list(ntrees = c(250, 500, 1000, 2000), max_depth = c(1,2,3), minobspernode = 10,
+  #             shrinkage = c(0.01))
+  # tune = list(ntrees = c(2000), max_depth = c(1), minobspernode = 10,
+  #             shrinkage = c(0.01))
+  # xgb_grid = SuperLearner::create.SL.xgboost(tune = tune, detailed_names = TRUE)
+  # fit_dum <- SuperLearner::SuperLearner(Y = Y3,
+  #                                   X = X3,
   #                                   family = binomial(),
-  #                                   SL.library = SL.library,
+  #                                   SL.library = xgb_grid$names,
   #                                   method = "method.NNLS",
   #                                   verbose = FALSE)
   #
   #
-  # get_stacked_pred <- function(t){
-  #   n_bins <- sum(t > trunc_time_grid)
-  #   new_stacked <- matrix(rep(t(newX), n_bins), ncol = ncol(newX) , byrow = TRUE)
-  #   dummy_stacked <- matrix(NA, ncol = length(trunc_time_grid), nrow = 1)
-  #   for (i in 1:n_bins){
-  #     dummies <- matrix(0, ncol = length(trunc_time_grid), nrow = nrow(newX))
-  #     if (t > trunc_time_grid[i]){
-  #       dummies[,i] <- 1
-  #     }
-  #     dummy_stacked <- rbind(dummy_stacked, dummies)
-  #   }
-  #   dummy_stacked <- dummy_stacked[-1,]
-  #   new_stacked <- cbind(dummy_stacked, new_stacked)
+  # # old and inefficient way of getting predictions
+  # # get_stacked_pred <- function(t){
+  # #   n_bins <- sum(t > trunc_time_grid)
+  # #   new_stacked <- matrix(rep(t(newX), n_bins), ncol = ncol(newX) , byrow = TRUE)
+  # #   dummy_stacked <- matrix(NA, ncol = length(trunc_time_grid), nrow = 1)
+  # #   for (i in 1:n_bins){
+  # #     dummies <- matrix(0, ncol = length(trunc_time_grid), nrow = nrow(newX))
+  # #     if (t > trunc_time_grid[i]){
+  # #       dummies[,i] <- 1
+  # #     }
+  # #     dummy_stacked <- rbind(dummy_stacked, dummies)
+  # #   }
+  # #   dummy_stacked <- dummy_stacked[-1,]
+  # #   new_stacked <- cbind(dummy_stacked, new_stacked)
+  # #   risk_set_names <- paste0("risk_set_", seq(1, (length(trunc_time_grid))))
+  # #   colnames(new_stacked) <- c(risk_set_names, colnames(newX))
+  # #   new_stacked <- data.frame(new_stacked)
+  # #   haz_preds <- predict(fit_dum, newdata=new_stacked)$pred
+  # #   haz_preds <- matrix(haz_preds, nrow = nrow(newX))
+  # #   surv_preds <- 1 - haz_preds
+  # #   total_surv_preds <- apply(surv_preds, prod, MARGIN = 1)
+  # #   return(total_surv_preds)
+  # # }
+  # # predictions_dum <- apply(X = matrix(newtimes), FUN = get_stacked_pred, MARGIN = 1)
+  #
+  # get_hazard_preds_SL_dum <- function(t){
+  #   dummies <- matrix(0, ncol = length(trunc_time_grid), nrow = nrow(newX))
+  #   index <- max(which(trunc_time_grid <= t))
+  #   dummies[,index] <- 1
+  #   new_stacked <- cbind(dummies, newX)
   #   risk_set_names <- paste0("risk_set_", seq(1, (length(trunc_time_grid))))
-  #   colnames(new_stacked) <- c(risk_set_names, colnames(newX))
-  #   new_stacked <- data.frame(new_stacked)
-  #   haz_preds <- predict(fit, newdata=new_stacked)$pred
-  #   haz_preds <- matrix(haz_preds, nrow = nrow(newX))
-  #   surv_preds <- 1 - haz_preds
-  #   total_surv_preds <- apply(surv_preds, prod, MARGIN = 1)
-  #   return(total_surv_preds)
+  #   colnames(new_stacked)[1:length(trunc_time_grid)] <- risk_set_names
+  #   preds <- predict(fit_dum, newdata=new_stacked)$pred
+  #   return(preds)
   # }
-
-  #predictions <- apply(X = matrix(newtimes), FUN = get_stacked_pred, MARGIN = 1)
+  #
+  # hazard_preds_SL_dum <- apply(X = matrix(trunc_time_grid), FUN = get_hazard_preds_SL_dum, MARGIN = 1)
+  #
+  # get_surv_preds_SL_dum <- function(t){
+  #   final_index <- max(which(trunc_time_grid <= t))
+  #   haz <- as.matrix(hazard_preds_SL_dum[,1:final_index])
+  #   anti_haz <- 1 - haz
+  #   surv <- apply(anti_haz, MARGIN = 1, prod)
+  #   return(surv)
+  # }
+  #
+  # surv_preds_SL_dum <- apply(X = matrix(newtimes), FUN = get_surv_preds_SL_dum, MARGIN = 1)
+  #
+  #
+  #
+  #
+  #
+  #
+  #
+  #
+  #
+  #
+  #
+  #
+  #
+  #
+  #
+  #
+  #
+  # # continuous time
+  #
+  # # this version does dummy variables for time - include this as an option
+  # ncol_stacked <- ncol(X) + 2
+  # stacked <- matrix(NA, ncol = ncol_stacked, nrow = 1)
+  # for (i in 1:(length(trunc_time_grid))){
+  #   risk_set <- dat[dat$time > time_grid[i],] # should this be <= rather than <??
+  #   risk_set_covariates <- risk_set[,1:ncol(X)]
+  #   event_indicators <- matrix(ifelse(risk_set$time <= time_grid[i + 1 ] & risk_set$event == 1, 1, 0))
+  #   time <- rep(time_grid[i + 1], length(event_indicators))
+  #   newdata <- as.matrix(cbind(time, risk_set_covariates, event_indicators))
+  #   stacked <- rbind(stacked, newdata)
+  # }
+  #
+  # stacked <- stacked[-1,]
+  # colnames(stacked)[1] <- "time"
+  # stacked <- data.frame(stacked)
+  # Y4 <- stacked$event_indicators
+  # X4 <- stacked[,-ncol(stacked)]
+  #
+  # # fit_cont <- SuperLearner::SuperLearner(Y = Y4,
+  # #                                   X = X4,
+  # #                                   family = binomial(),
+  # #                                   SL.library = SL.library,
+  # #                                   method = "method.NNLS",
+  # #                                   verbose = FALSE)
+  # fit_cont <- SuperLearner::SuperLearner(Y = Y4,
+  #                                       X = X4,
+  #                                       family = binomial(),
+  #                                       SL.library = xgb_grid$names,
+  #                                       method = "method.NNLS",
+  #                                       verbose = FALSE)
+  #
+  # ##### old version of prediction function - gives same results as other below
+  # # get_stacked_pred <- function(t){
+  # #   #n_bins <- sum(t > trunc_time_grid) # this only makes sense for discrete time!
+  # #   n_bins <- sum(t >= time_grid_approx)
+  # #   new_stacked <- matrix(rep(t(newX), n_bins), ncol = ncol(newX) , byrow = TRUE)
+  # #   dummy_stacked <- matrix(NA, ncol = 1,nrow = 1)# length(trunc_time_grid), nrow = 1)
+  # #   for (i in 1:n_bins){
+  # #     dummies <- as.matrix(rep(time_grid_approx[i], nrow(newX)))
+  # #     dummy_stacked <- rbind(dummy_stacked, dummies)
+  # #   }
+  # #   dummy_stacked <- dummy_stacked[-1,]
+  # #   new_stacked <- cbind(dummy_stacked, new_stacked)
+  # #   colnames(new_stacked) <- c("time", colnames(newX))
+  # #   new_stacked <- data.frame(new_stacked)
+  # #   haz_preds <- predict(fit_cont, newdata=new_stacked)$pred
+  # #   haz_preds <- matrix(haz_preds, nrow = nrow(newX))
+  # #   surv_preds <- 1 - haz_preds
+  # #   total_surv_preds <- apply(surv_preds, prod, MARGIN = 1)
+  # #   return(total_surv_preds)
+  # # }
+  # # predictions_cont <- apply(X = matrix(newtimes), FUN = get_stacked_pred, MARGIN = 1)
+  #
+  # get_hazard_preds_SL_cont <- function(t){
+  #   new_stacked <- as.matrix(data.frame(time = t, newX))
+  #   preds <- predict(fit_cont, newdata=new_stacked)$pred
+  #   return(preds)
+  # }
+  #
+  # hazard_preds_SL_cont <- apply(X = matrix(time_grid_approx), FUN = get_hazard_preds_SL_cont, MARGIN = 1)
+  #
+  # get_surv_preds_SL_cont <- function(t){
+  #   final_index <- max(which(time_grid_approx <= t))
+  #   haz <- as.matrix(hazard_preds_SL_cont[,1:final_index])
+  #   anti_haz <- 1 - haz
+  #   surv <- apply(anti_haz, MARGIN = 1, prod)
+  #   return(surv)
+  # }
+  #
+  # surv_preds_SL_cont <- apply(X = matrix(newtimes), FUN = get_surv_preds_SL_cont, MARGIN = 1)
 
   res <- list(S_T_preds = surv_preds,
+              S_T_preds2 = surv_preds2,
               fit = fit)
   class(res) <- "stackSurv"
   return(res)
