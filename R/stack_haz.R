e84d589653a3015141572fd1f7ea56f4ca9511d8
cwolock <cwolock@gmail.com>
2022-07-13 22:21:56 -0800

    new convention for time discretization

--- R/stack_haz.R
+++ R/stack_haz.R
@@ -7,18 +7,21 @@ stack_haz <- function(time, event, X, time_grid, entry, time_basis){
   dat <- data.frame(X, event = event, time = time)
 
   if (time_basis == "continuous"){
-    # we will treat time as continuous
     ncol_stacked <- ncol(X) + 2 # covariates, time, binary outcome
     stacked <- matrix(NA, ncol = ncol_stacked, nrow = 1)
     for (i in 1:(length(trunc_time_grid))){ # can change this to not do anything in last time bin
       if (is.null(entry)){ # no entry (truncation variable) given
-        risk_set <- dat[dat$time > time_grid[i],]
+        #risk_set <- dat[dat$time > time_grid[i],]
+        risk_set <- dat[dat$time >= time_grid[i],]
       } else{ # entry given
-        risk_set <- dat[dat$time > time_grid[i] & entry <= time_grid[i+1],]
+        #risk_set <- dat[dat$time > time_grid[i] & entry <= time_grid[i+1],]
+        risk_set <- dat[dat$time >= time_grid[i] & entry <= time_grid[i],]
       }
       risk_set_covariates <- risk_set[,1:ncol(X)]
-      event_indicators <- matrix(ifelse(risk_set$time <= time_grid[i + 1 ] & risk_set$event == 1, 1, 0))
-      t <- rep(time_grid[i + 1], nrow(event_indicators))
+      #event_indicators <- matrix(ifelse(risk_set$time <= time_grid[i + 1 ] & risk_set$event == 1, 1, 0))
+      event_indicators <- matrix(ifelse(risk_set$time < time_grid[i + 1 ] & risk_set$event == 1, 1, 0))
+      #t <- rep(time_grid[i + 1], nrow(event_indicators))
+      t <- rep(time_grid[i], nrow(event_indicators))
       newdata <- as.matrix(cbind(t, risk_set_covariates, event_indicators))
       stacked <- rbind(stacked, newdata)
     }
