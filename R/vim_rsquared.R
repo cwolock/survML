b6607e52186199a196546f2eaa49b13a9960f7d6
Charlie Wolock <cwolock@gmail.com>
2024-05-30 14:10:43 -0400

    added landmark time rsquared vim

--- R/vim_rsquared.R
+++ R/vim_rsquared.R
@@ -1,4 +1,4 @@
-#' Estimate AUC VIM
+#' Estimate Brier score VIM
 #'
 #' @param time \code{n x 1} numeric vector of observed
 #' follow-up times If there is censoring, these are the minimum of the
@@ -8,7 +8,7 @@
 #' @param approx_times Numeric vector of length J1 giving times at which to
 #' approximate integrals.
 #' @param landmark_times Numeric vector of length J2 giving
-#' times at which to estimate AUC
+#' times at which to estimate Brier score
 #' @param f_hat Full oracle predictions (n x J1 matrix)
 #' @param fs_hat Residual oracle predictions (n x J1 matrix)
 #' @param S_hat Estimates of conditional event time survival function (n x J2 matrix)
@@ -17,34 +17,32 @@
 #' @param sample_split Logical indicating whether or not to sample split
 #' @param ss_folds Numeric vector of length n giving sample-splitting folds
 #' @param scale_est Logical, whether or not to force the VIM estimate to be nonnegative
-#' @param robust Logical, whether or not to use the doubly-robust debiasing approach. This option
-#' is meant for illustration purposes only --- it should be left as \code{TRUE}.
 #'
 #' @return data frame giving results
 #'
 #' @export
 
-vim_AUC <- function(time,
-                    event,
-                    approx_times,
-                    landmark_times,
-                    f_hat,
-                    fs_hat,
-                    S_hat,
-                    G_hat,
-                    folds,
-                    sample_split,
-                    ss_folds,
-                    robust = TRUE,
-                    scale_est = FALSE){
+vim_rsquared <- function(time,
+                         event,
+                         approx_times,
+                         landmark_times,
+                         f_hat,
+                         fs_hat,
+                         S_hat,
+                         G_hat,
+                         folds,
+                         ss_folds,
+                         sample_split,
+                         scale_est = FALSE){
 
   J1 <- length(landmark_times)
   V <- length(unique(folds))
+
   one_step <- rep(NA, J1)
   var_est <- rep(NA, J1)
-  full_one_step <- rep(NA, J1)
-  reduced_one_step <- rep(NA, J1)
+  # full_one_step <- rep(NA, J1)
   # full_plug_in <- rep(NA, J1)
+  # reduced_one_step <- rep(NA, J1)
   # reduced_plug_in <- rep(NA, J1)
   cil <- rep(NA, J1)
   ciu <- rep(NA, J1)
@@ -54,7 +52,16 @@ vim_AUC <- function(time,
     tau <- landmark_times[i]
     # CV_full_plug_ins <- rep(NA, V)
     # CV_reduced_plug_ins <- rep(NA, V)
+    # CV_full_one_steps <- rep(NA, V)
+    # CV_reduced_one_steps <- rep(NA, V)
+    CV_one_steps <- rep(NA, V)
+    # CV_plug_ins <- rep(NA, V)
     CV_var_ests <- rep(NA, V)
+    split_one_step_fulls <- rep(NA, V)
+    # split_plug_in_fulls <- rep(NA, V)
+    split_one_step_reduceds <- rep(NA, V)
+    # split_plug_in_reduceds <- rep(NA, V)
+
     CV_full_numerators <- rep(NA, V)
     CV_reduced_numerators <- rep(NA, V)
     CV_denominators <- rep(NA, V)
@@ -62,33 +69,38 @@ vim_AUC <- function(time,
     split_denominator_fulls <- rep(NA, V)
     split_numerator_reduceds <- rep(NA, V)
     split_denominator_reduceds <- rep(NA, V)
-    # split_plug_in_fulls <- rep(NA, V)
-    # split_plug_in_reduceds <- rep(NA, V)
     split_var_est_fulls <- rep(NA, V)
     split_var_est_reduceds <- rep(NA, V)
-    for (j in 1:V){
+
+    for (j in 1:length(unique(folds))){
       time_holdout <- time[folds == j]
       event_holdout <- event[folds == j]
-
-      V_0 <- estimate_AUC(time = time_holdout,
-                          event = event_holdout,
-                          approx_times = approx_times,
-                          tau = tau,
-                          preds = f_hat[[j]][,i],
-                          S_hat = S_hat[[j]],
-                          G_hat = G_hat[[j]],
-                          robust = robust)
-      V_0s <- estimate_AUC(time = time_holdout,
-                           event = event_holdout,
-                           approx_times = approx_times,
-                           tau = tau,
-                           preds = fs_hat[[j]][,i],
-                           S_hat = S_hat[[j]],
-                           G_hat = G_hat[[j]],
-                           robust = robust)
-
+      V_0 <- estimate_rsquared(time = time_holdout,
+                               event = event_holdout,
+                               approx_times = approx_times,
+                               tau = tau,
+                               preds = f_hat[[j]][,i],
+                               S_hat = S_hat[[j]],
+                               G_hat = G_hat[[j]])
+      V_0s <- estimate_rsquared(time = time_holdout,
+                                event = event_holdout,
+                                approx_times = approx_times,
+                                tau = tau,
+                                preds = fs_hat[[j]][,i],
+                                S_hat = S_hat[[j]],
+                                G_hat = G_hat[[j]])
+      # CV_full_one_steps[j] <- V_0$one_step
       # CV_full_plug_ins[j] <- V_0$plug_in
+      # CV_reduced_one_steps[j] <- V_0s$one_step
       # CV_reduced_plug_ins[j] <- V_0s$plug_in
+      CV_one_steps[j] <-  V_0$one_step -V_0s$one_step
+      # CV_plug_ins[j] <-  V_0$plug_in -V_0s$plug_in
+      split_one_step_fulls[j] <- V_0$one_step
+      # split_plug_in_fulls[j] <- V_0$plug_in
+      split_one_step_reduceds[j] <- V_0s$one_step
+      # split_plug_in_reduceds[j] <- V_0s$plug_in
+
+      # added these after adding standardization
       CV_full_numerators[j] <- V_0$numerator
       CV_reduced_numerators[j] <- V_0s$numerator
       CV_denominators[j] <- V_0$denominator
@@ -96,8 +108,6 @@ vim_AUC <- function(time,
       split_denominator_fulls[j] <- V_0$denominator
       split_numerator_reduceds[j] <- V_0s$numerator
       split_denominator_reduceds[j] <- V_0s$denominator
-      # split_plug_in_fulls[j] <- V_0$plug_in
-      # split_plug_in_reduceds[j] <- V_0s$plug_in
       split_var_est_fulls[j] <- mean(V_0$EIF^2)
       split_var_est_reduceds[j] <- mean(V_0s$EIF^2)
       EIF <- V_0$EIF - V_0s$EIF
@@ -107,19 +117,21 @@ vim_AUC <- function(time,
     if (sample_split){
       folds_0 <- sort(unique(folds[ss_folds == 0]))
       folds_1 <- sort(unique(folds[ss_folds == 1]))
+
       one_step[i] <- mean(split_numerator_fulls[folds_0])/mean(split_denominator_fulls[folds_0]) -
         mean(split_numerator_reduceds[folds_1])/mean(split_denominator_reduceds[folds_1])
-      # full_one_step[i] <- mean(split_numerator_fulls[folds_0])/mean(split_denominator_fulls[folds_0])
+      # full_one_step[i] <- mean(split_one_step_fulls[folds_0])
       # full_plug_in[i] <- mean(split_plug_in_fulls[folds_0])
-      # reduced_one_step[i] <- mean(split_numerator_reduceds[folds_1])/mean(split_denominator_reduceds[folds_1])
+      # reduced_one_step[i] <- mean(split_one_step_reduceds[folds_1])
       # reduced_plug_in[i] <- mean(split_plug_in_reduceds[folds_1])
       var_est[i] <- mean(split_var_est_fulls[folds_0]) +
         mean(split_var_est_reduceds[folds_1])
     } else{
-      one_step[i] <- mean(CV_full_numerators - CV_reduced_numerators)/mean(CV_denominators)#mean(CV_one_steps)
+
+      one_step[i] <- mean(CV_full_numerators - CV_reduced_numerators)/mean(CV_denominators)
       var_est[i] <- mean(CV_var_ests)
-      # full_one_step[i] <- mean(CV_full_numerators)/mean(CV_denominators)#mean(CV_full_one_steps)
-      # reduced_one_step[i] <- mean(CV_reduced_numerators)/mean(CV_denominators)#mean(CV_reduced_one_steps)
+      # full_one_step[i] <- mean(CV_full_one_steps)
+      # reduced_one_step[i] <- mean(CV_reduced_one_steps)
       # full_plug_in[i] <- mean(CV_full_plug_ins)
       # reduced_plug_in[i] <- mean(CV_reduced_plug_ins)
     }
