5a57b6629a1dc84d9795a5eb9cc132720d8d1e39
Charlie Wolock <cwolock@gmail.com>
2024-10-10 13:58:05 -0400

    bug fix

--- R/DR_pseudo_outcome_regression.R
+++ R/DR_pseudo_outcome_regression.R
@@ -10,6 +10,7 @@ DR_pseudo_outcome_regression <- function(time,
                                          S_hat,
                                          G_hat,
                                          newtimes,
+                                         outcome,
                                          approx_times,
                                          SL.library,
                                          V){
@@ -20,16 +21,33 @@ DR_pseudo_outcome_regression <- function(time,
     Y_t <- pmin(time, tau)
 
     # calculate m_0 at a specific time t
-    calc_one <- function(t){
-      if (approx_times[t] >= tau){
-        m <-  rep(1, nrow(S_hat))
-      } else{
-        m <- S_hat[,which(approx_times == tau)]/S_hat[,t]
+    calc_one <- function(t, outcome){
+      if (outcome == "survival_probability"){
+        if (approx_times[t] >= tau){
+          m <-  rep(1, nrow(S_hat))
+        } else{
+          m <- S_hat[,which(approx_times == tau)]/S_hat[,t]
+        }
+      } else if (outcome == "restricted_survival_time"){
+        if (approx_times[t] >= tau){
+          m <- rep(tau, nrow(S_hat))
+        } else{
+          int.vals <- t(sapply(1:nrow(S_hat), function(i) {
+            indices <- which(approx_times <= tau & approx_times >= t)
+            S_hat_ind <- S_hat[,indices]
+            approx_times_ind <- approx_times[indices]
+            vals <- diff(S_hat_ind[i,])*approx_times_ind[-length(approx_times_ind)]
+            return(sum(vals))
+          }))
+          m1 <- -int.vals
+          m2 <- tau * S_hat[,which(approx_times == tau)]
+          m <- (m1 + m2)/S_hat[,t]
+        }
       }
       return(m)
     }
     # calculate m_0 over the whole grid
-    ms <- matrix(unlist(lapply(1:length(approx_times), FUN = calc_one)),
+    ms <- matrix(unlist(lapply(1:length(approx_times), FUN = calc_one, outcome = outcome)),
                  nrow = length(time))
 
     m_Y <- apply(X = matrix(1:length(Y_t)), MARGIN = 1,
